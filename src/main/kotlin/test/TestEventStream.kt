8128bc04b632947d1ff6850508a2a026f9b32018
Arnaud ESTEVE <arnaud.esteve@decathlon.com>
2019-08-12 14:45:34 +0200

    parse JSON output from Stream

--- src/main/kotlin/test/TestEventStream.kt
+++ src/main/kotlin/test/TestEventStream.kt
@@ -1,7 +1,9 @@
 package test
 
 import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
 import io.vertx.core.http.HttpClientOptions
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.client.WebClient
 import io.vertx.ext.web.client.WebClientOptions
 
@@ -16,8 +18,15 @@ object TestEventStream {
 
         client.get("stream.meetup.com", "/2/rsvps") { resp ->
             println(resp.statusCode())
+            var receiving = Buffer.buffer()
             resp.handler { buff ->
-                println(buff)
+                val str = buff.toString("UTF-8")
+                receiving.appendBuffer(buff)
+                if (receiving.toString().endsWith("\n")) {
+                    val json = JsonObject(receiving)
+                    println(json)
+                    receiving = Buffer.buffer()
+                }
             }
         }
         .setChunked(true)
